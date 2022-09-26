0a9d2030b5173381a5b21ae4e27c83045c988c8e
jorgecortez <jorgelcortez2114@gmail.com>
2022-09-26 16:57:10 -0300

    correcciones de entrega

--- js/script.js
+++ js/script.js
@@ -12,6 +12,7 @@ const listaNombres = []
 let btnCrearUsuario = document.querySelector("button")
 let inputs = document.querySelectorAll("#crearTabla input")
 
+
 btnCrearUsuario.addEventListener("click", () => {
     let nombre = inputs[0].value
     let nota = inputs[1].value
@@ -21,41 +22,11 @@ btnCrearUsuario.addEventListener("click", () => {
     } else if (nota > 10 || nota < 1) {
         alert("¡Calificación incorrecta!")
     } else {
-        listaNombres.push(new Usuario(nombre, nota))
-        console.log(listaNombres)
+        const nuevoUsuario = new Usuario (nombre, nota)
+        listaNombres.push(nuevoUsuario)
+        let alumnos = document.createElement("ol")
+        alumnos.innerHTML = `Alumno: ${nuevoUsuario.nombre}   /   Nota: ${nuevoUsuario.nota}`
+        document.body.appendChild(alumnos)
         return
     }
 })
-
-for (let nombre of listaNombres) {
-    let nombres = document.createElement("h3")
-    nombres.innerHTML = nombre
-    document.body.appendChild(nombres)
-}
-
-
-/*
-let desicion = Number(prompt("¿Qué desea hacer?\n1.Agregar alumno.\n2.Finalizar"))
-while (desicion == 1) {
-
-    let nombre = prompt("Ingrese un Nombre:")
-    let nota = Number(prompt("Ingrese la Nota:"))
-    let aprobado
-    if (nota >= "6") {
-        aprobado = true
-    } else {
-        aprobado = false
-    }
-
-    listaNombres.push(nombre, nota, aprobado)
-    desicion = Number(prompt("¿Qué desea hacer?\n1.Agregar alumno.\n2.Finalizar"))
-}
-
-console.log(listaNombres)
-
-for (let nombre of listaNombres) {
-    let nombres = document.createElement("h1")
-    nombres.innerHTML = nombre
-    document.body.appendChild(nombres)
-}
-*/
